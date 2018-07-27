7d1541012717da4aa99aad854ac2bd511fec951c
Starostin Evgenii <estarostin@bellintegrator.com>
2018-07-27 17:18:49 +0300

    -

--- js/script.js
+++ js/script.js
@@ -1,6 +1,59 @@
-document.querySelector('.scroll.up').addEventListener('click', scroll);
+/* --- Карусель в разделе "Главное" --- */
+(function () {
+    "use strict";
+    /* конфигурация */
+    var height = 135, // высота блока
+        count = 2, // количество блоков
 
-function scroll() {
-    var devices = document.querySelector('.devices');
-    devices.style.marginTop = parseFloat(devices.style.marginTop) - 130 + 'px';
-}
+        carousel = document.getElementById('carousel'),
+        list = carousel.querySelector('ul'),
+        listElems = carousel.querySelectorAll('li'),
+
+        position = 0; // текущий сдвиг вверх
+
+    carousel.querySelector('.prev').onclick = function () {
+        // сдвиг вниз
+        // последнее передвижение вниз может быть не на 3, а на 2 или 1 элемент
+        position = Math.min(position + height * count, 0);
+        list.style.marginTop = position + 'px';
+    };
+
+    carousel.querySelector('.next').onclick = function () {
+        // сдвиг вверх
+        // последнее передвижение вверх может быть не на 3, а на 2 или 1 элемент
+        position = Math.max(position - height * count, -height * (listElems.length - count));
+        list.style.marginTop = position + 'px';
+    };
+}());
+/* --- Карусель в разделе "Главное" --- */
+
+
+
+/* --- Карусель в разделе "Избранные сценарии" --- */
+(function () {
+    "use strict";
+    /* конфигурация */
+    var height = 115, // высота блока
+        count = 3, // количество блоков
+
+        carousel = document.getElementById('slider-fav'),
+        list = carousel.querySelector('ul'),
+        listElems = carousel.querySelectorAll('li'),
+
+        position = 0; // текущий сдвиг вверх
+
+    document.querySelector('.fav-section .prev').onclick = function () {
+        // сдвиг вниз
+        // последнее передвижение вниз может быть не на 3, а на 2 или 1 элемент
+        position = Math.min(position + height * count, 0);
+        list.style.marginTop = position + 'px';
+    };
+
+    document.querySelector('.fav-section .next').onclick = function () {
+        // сдвиг вверх
+        // последнее передвижение вверх может быть не на 3, а на 2 или 1 элемент
+        position = Math.max(position - height * count, -height * (listElems.length - count));
+        list.style.marginTop = position + 'px';
+    };
+}());
+/* --- Карусель в разделе "Избранные сценарии" --- */

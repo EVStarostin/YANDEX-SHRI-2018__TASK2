b6839f8f0e27ed7ab5d5d1400e14f78b805780c1
Evgenii Starostin <e.starostin89@gmail.com>
2018-07-29 16:07:10 +0300

    -

--- termo/js/script.js
+++ termo/js/script.js
@@ -1,59 +1,124 @@
-/* --- Карусель в разделе "Главное" --- */
-(function () {
-    "use strict";
-    /* конфигурация */
-    var height = 135, // высота блока
-        count = 2, // количество блоков
+// Константы
+var MIN_ANGLE = 30,
+    MAX_ANGLE = 330,
+    MIN_TEMPERATURE = 10,
+    MAX_TEMPERATURE = 25;
 
-        carousel = document.getElementById('carousel'),
-        list = carousel.querySelector('ul'),
-        listElems = carousel.querySelectorAll('li'),
+// Функция для отображения температуры на регуляторе
+// Первым аргументом принимает элемент, а вторым угол
+function displayTemperature(element, angle) {
+    var temp = MIN_TEMPERATURE + (MAX_TEMPERATURE - MIN_TEMPERATURE) * (angle - MIN_ANGLE) / (MAX_ANGLE - MIN_ANGLE);
+    element.dataset.after = '+' + Math.round(temp);
+}
 
-        position = 0; // текущий сдвиг вверх
+// Функция для получения угла поворота элемента
+// Первым аргументом принимает элемент
+function getAngle(element) {
+    var valueStyle = window.getComputedStyle(element).getPropertyValue("transform");
+    if (valueStyle === 'none') {
+        return 0;
+    }
 
-    carousel.querySelector('.prev').onclick = function () {
-        // сдвиг вниз
-        // последнее передвижение вниз может быть не на 3, а на 2 или 1 элемент
-        position = Math.min(position + height * count, 0);
-        list.style.marginTop = position + 'px';
+    var values = valueStyle.split('(')[1].split(')')[0].split(',');
+    var cos = values[0],
+        sin = values[1];
+    var degree = Math.round(Math.asin(sin) * (180 / Math.PI));
+
+    if (cos < 0) {
+        var addDegree = 90 - Math.round(Math.asin(sin) * (180 / Math.PI));
+        degree = 90 + addDegree;
+    }
+    if (degree < 0) {
+        degree = 360 + degree;
+    }
+
+    return degree;
+}
+
+// Функция для задания свойства transform: rotate всем нужным блокам
+// Первым аргументом принимает элемент, вторым - угол поворот
+function rotate(element, deg) {
+    element.style.transform = 'rotate(' + deg + 'deg)';
+}
+
+// Функция для получения угла относительно центра по координатам курсора
+// Первым аргументом принимает событие
+function getCursorAngle(position) {
+    var roundSlider = document.querySelector('.round-slider');
+    var center = {
+        x: roundSlider.clientWidth / 2,
+        y: roundSlider.clientHeight / 2
     };
+    var x = position.x - center.x,
+        y = position.y - center.y;
 
-    carousel.querySelector('.next').onclick = function () {
-        // сдвиг вверх
-        // последнее передвижение вверх может быть не на 3, а на 2 или 1 элемент
-        position = Math.max(position - height * count, -height * (listElems.length - count));
-        list.style.marginTop = position + 'px';
-    };
-}());
-/* --- Карусель в разделе "Главное" --- */
+    if (x === 0) {
+        return (y > 0) ? 180 : 0;
+    }
+    var a = Math.atan(y / x) * 180 / Math.PI;
+    a = (x > 0) ? a + 90 : a + 270;
+    a = a + 180;
+    return a;
+}
 
+// Функция-обработчик перемещения мыши на крутилке
+// Первым аргументом принимает событие.
+function handleMouseMove(e) {
+    var roundSlider = document.querySelector('.round-slider'),
+        block2 = document.querySelector('.block-2'),
+        block3 = document.querySelector('.block-3'),
+        pointer = document.querySelector('.pointer');
 
+    var prevPositionAngle = getCursorAngle({
+            x: e.offsetX - e.movementX,
+            y: e.offsetY - e.movementY
+        }),
+        positionAngle = getCursorAngle({
+            x: e.offsetX,
+            y: e.offsetY
+        });
 
-/* --- Карусель в разделе "Избранные сценарии" --- */
-(function () {
-    "use strict";
-    /* конфигурация */
-    var height = 115, // высота блока
-        count = 3, // количество блоков
+    var angle = (positionAngle - prevPositionAngle);
+    
+    if (Math.abs(angle) > 100) {
+        return;
+    } else if (getAngle(block3) + angle <= 330 && getAngle(block3) + angle >= 30) {
+        rotate(block2, (getAngle(block2) + angle));
+        rotate(block3, (getAngle(block3) + angle));
+        rotate(pointer, (getAngle(pointer) + angle));
+        displayTemperature(roundSlider, getAngle(block3));
+    } else if (getAngle(block3) + angle > 330) {
+        rotate(block2, MAX_ANGLE - 180);
+        rotate(block3, MAX_ANGLE);
+        rotate(pointer, MAX_ANGLE);
+        displayTemperature(roundSlider, getAngle(block3));
+        return;
+    } else if (getAngle(block3) + angle < 30) {
+        rotate(block2, MIN_ANGLE + 180);
+        rotate(block3, MIN_ANGLE);
+        rotate(pointer, MIN_ANGLE);
+        displayTemperature(roundSlider, getAngle(block3));
+        return;
+    } else {
+        return;
+    }
 
-        carousel = document.getElementById('slider-fav'),
-        list = carousel.querySelector('ul'),
-        listElems = carousel.querySelectorAll('li'),
+    if (getAngle(block3) < 180) {
+        block2.style.opacity = '0';
+        block3.style.opacity = '1';
+    } else {
+        block2.style.opacity = '1';
+        block3.style.opacity = '0';
+    }
+}
 
-        position = 0; // текущий сдвиг вверх
+var roundSlider = document.querySelector('.round-slider');
+displayTemperature(roundSlider, getAngle(document.querySelector('.block-3')));
 
-    document.querySelector('.fav-section .prev').onclick = function () {
-        // сдвиг вниз
-        // последнее передвижение вниз может быть не на 3, а на 2 или 1 элемент
-        position = Math.min(position + height * count, 0);
-        list.style.marginTop = position + 'px';
-    };
+roundSlider.addEventListener('mousedown', function () {
+    roundSlider.addEventListener('mousemove', handleMouseMove);
+});
+document.addEventListener('mouseup', function () {
+    roundSlider.removeEventListener('mousemove', handleMouseMove);
+});
 
-    document.querySelector('.fav-section .next').onclick = function () {
-        // сдвиг вверх
-        // последнее передвижение вверх может быть не на 3, а на 2 или 1 элемент
-        position = Math.max(position - height * count, -height * (listElems.length - count));
-        list.style.marginTop = position + 'px';
-    };
-}());
-/* --- Карусель в разделе "Избранные сценарии" --- */

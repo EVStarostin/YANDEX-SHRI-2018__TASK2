4cef74cf95a0bf12628ae6b07b7130751b384270
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-02 11:33:58 +0300

    -

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.devices').addEventListener('scroll', function(e) {
+document.querySelector('.devices').addEventListener('scroll', function (e) {
     if (this.scrollTop !== 0) {
         this.classList.add('scrolled');
     } else {

2bb9a92b01dd6d0e82c7fe8953e69959385a4685
Evgenii Starostin <e.starostin89@gmail.com>
2018-08-02 18:43:39 +0300

    -

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.devices').addEventListener('scroll', function (e) {
+    if (this.scrollTop !== 0) {
+        this.classList.add('scrolled');
+    } else {
+        this.classList.remove('scrolled');   
+    }
+});
+
+document.querySelector('.menu-btn').addEventListener('click', function (e) {
+    document.querySelector('.menu').classList.toggle('visible');
+});

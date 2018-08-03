ca5d8c73645f4db9db4e1883fc009e99f45dde10
Evgenii Starostin <e.starostin89@gmail.com>
2018-08-03 23:28:48 +0300

    -

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,5 @@ document.querySelector('.devices').addEventListener('scroll', function (e) {
 
 document.querySelector('.menu-btn').addEventListener('click', function (e) {
     document.querySelector('.menu').classList.toggle('visible');
+    document.querySelector('.header-and-main').classList.toggle('menu-visible');
 });

79d21b661024c0998b95796cac9da949a10d22aa
Evgenii Starostin <e.starostin89@gmail.com>
2018-08-04 16:52:27 +0300

    добавил модальное окно регулировки температуры

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,13 @@ document.querySelector('.menu-btn').addEventListener('click', function (e) {
     document.querySelector('.menu').classList.toggle('visible');
     document.querySelector('.header-and-main').classList.toggle('menu-visible');
 });
+
+document.querySelector('.close-modal').addEventListener('click', function(e) {
+    document.querySelector('.modal').classList.remove('visible');
+    document.body.classList.remove('blur');
+});
+
+document.querySelector('.save-modal').addEventListener('click', function(e) {
+    document.querySelector('.modal').classList.remove('visible');
+    document.body.classList.remove('blur');
+});

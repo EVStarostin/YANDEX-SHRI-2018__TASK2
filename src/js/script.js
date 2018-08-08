2f0216240a88226ae6857c709c23952266eea8cb
Evgenii Starostin <e.starostin89@gmail.com>
2018-08-08 21:49:04 +0300

    по нажатию на избранное устройство открывается модальное окно

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 /* Показывать/прятать двойную стрелку вверх при скролле плашек с устройствами в разделе главное */
-document.querySelector('.devices').addEventListener('scroll', function (e) {
+document.querySelector('.devices').addEventListener('scroll', function () {
     if (this.scrollTop !== 0) {
         this.classList.add('scrolled');
     } else {
@@ -8,39 +8,15 @@ document.querySelector('.devices').addEventListener('scroll', function (e) {
 });
 
 /* Показывать/прятать меню при клике по кнопке */
-document.querySelector('.menu-btn').addEventListener('click', function (e) {
+document.querySelector('.menu-btn').addEventListener('click', function () {
     document.querySelector('.menu').classList.toggle('visible');
     document.querySelector('.header-and-main').classList.toggle('menu-visible');
 });
 
-/* Закрывать модальное окно регулировки температуры по кнопке Закрыть */
-document.querySelector('.temp-modal .close-modal').addEventListener('click', function(e) {
-    document.querySelector('.temp-modal').classList.remove('visible');
-    document.body.classList.remove('blur');
-});
-
-/* Закрывать модальное окно регулировки температуры по кнопке Применить */
-document.querySelector('.temp-modal .save-modal').addEventListener('click', function(e) {
-    document.querySelector('.temp-modal').classList.remove('visible');
-    document.body.classList.remove('blur');
-});
-
-/* Закрывать модальное окно регулировки света по кнопке Закрыть */
-document.querySelector('.light-modal .close-modal').addEventListener('click', function(e) {
-    document.querySelector('.light-modal').classList.remove('visible');
-    document.body.classList.remove('blur');
-});
-
-/* Закрывать модальное окно регулировки света по кнопке Применить */
-document.querySelector('.light-modal .save-modal').addEventListener('click', function(e) {
-    document.querySelector('.light-modal').classList.remove('visible');
-    document.body.classList.remove('blur');
-});
-
 /* Фильтр в разделе избранные устройства */
-var filterButtons = document.querySelectorAll('.section-fav-devides .filter button');
+var filterButtons = document.querySelectorAll('.section-fav-devices .filter button');
 for (var i = 0; i < filterButtons.length; i++) {
-    filterButtons[i].addEventListener('click', function(e) {
+    filterButtons[i].addEventListener('click', function() {
         /* Добавляем нажатой кнопке класс active, у остальных кнопок удаляем */
         this.classList.add('active');
         for (var j = 0; j < filterButtons.length; j++) {
@@ -49,7 +25,7 @@ for (var i = 0; i < filterButtons.length; i++) {
             }
         }
         /* Прячем все устройства, у которых атрубит data-filter не равен атрибуту кликнутой кнопки */
-        var devices = document.querySelectorAll('.section-fav-devides .fav-devides li');
+        var devices = document.querySelectorAll('.section-fav-devices .fav-devices li');
         for (var k = 0; k < devices.length; k++) {
             if (this.dataset.filter === 'Все') {
                 devices[k].style.display = "block";
@@ -60,49 +36,49 @@ for (var i = 0; i < filterButtons.length; i++) {
             }
         }
         // Меняем прозрачность стрелки вправо, если все устройства помещаются на экране
-        if (document.querySelector('.fav-devides').clientWidth < document.querySelector('.fav-devides').scrollWidth) {
-            document.querySelector('.section-fav-devides .right').style.opacity = '1';
+        if (document.querySelector('.fav-devices').clientWidth < document.querySelector('.fav-devices').scrollWidth) {
+            document.querySelector('.section-fav-devices .right').style.opacity = '1';
         } else {
-            document.querySelector('.section-fav-devides .right').style.opacity = '.3';
+            document.querySelector('.section-fav-devices .right').style.opacity = '.3';
         }
     });
 }
 
 // Меняем прозрачность стрелки вправо, если устройства не помещаются на экране
-window.addEventListener('load', function(e) {
-    if (document.querySelector('.fav-devides').clientWidth < document.querySelector('.fav-devides').scrollWidth) {
-        document.querySelector('.section-fav-devides .right').style.opacity = '1';
+window.addEventListener('load', function() {
+    if (document.querySelector('.fav-devices').clientWidth < document.querySelector('.fav-devices').scrollWidth) {
+        document.querySelector('.section-fav-devices .right').style.opacity = '1';
     }
 })
 
 /* Обработка нажатия на кнопку влево в разделе избранные устройства */
-document.querySelector('.section-fav-devides .left').addEventListener('click', function(e) {
-    document.querySelector('.fav-devides').scrollLeft -= 215;
+document.querySelector('.section-fav-devices .left').addEventListener('click', function() {
+    document.querySelector('.fav-devices').scrollLeft -= 215;
 });
 
 /* Обработка нажатия на кнопку вправо в разделе избранные устройства */
-document.querySelector('.section-fav-devides .right').addEventListener('click', function(e) {
-    document.querySelector('.fav-devides').scrollLeft += 215;
+document.querySelector('.section-fav-devices .right').addEventListener('click', function() {
+    document.querySelector('.fav-devices').scrollLeft += 215;
 });
 
 // При скролле меняем прозрачность кнопок если дальше скролить нельзя
-document.querySelector('.fav-devides').addEventListener('scroll', function(e) {
+document.querySelector('.fav-devices').addEventListener('scroll', function() {
     if (this.scrollLeft <= 0) {
-        document.querySelector('.section-fav-devides .left').style.opacity = '.3';
+        document.querySelector('.section-fav-devices .left').style.opacity = '.3';
     } else {
-        document.querySelector('.section-fav-devides .left').style.opacity = '1';
+        document.querySelector('.section-fav-devices .left').style.opacity = '1';
     }
     if (this.clientWidth + this.scrollLeft >= this.scrollWidth) {
-        document.querySelector('.section-fav-devides .right').style.opacity = '.3';
+        document.querySelector('.section-fav-devices .right').style.opacity = '.3';
     } else {
-        document.querySelector('.section-fav-devides .right').style.opacity = '1';
+        document.querySelector('.section-fav-devices .right').style.opacity = '1';
     }
 });
 
 /* Обработка выбора режима в модальном окне */
 var modes = document.querySelectorAll('.modal .mode button');
 for (var i = 0; i < modes.length; i++) {
-    modes[i].addEventListener('click', function(e) {
+    modes[i].addEventListener('click', function() {
         /* Добавляем нажатой кнопке класс active, у остальных кнопок удаляем */
         this.classList.add('active');
         for (var j = 0; j < modes.length; j++) {
@@ -112,3 +88,60 @@ for (var i = 0; i < modes.length; i++) {
         }
     });
 }
+
+/* При нажатии на плашку с устройством в разделе Избранные устройства открывается 
+модальное окно, куда подставляется название устройства, статус работы и иконка */
+var favDevices = document.querySelectorAll('.fav-devices button');
+for (var i = 0; i < favDevices.length; i++) {
+    favDevices[i].addEventListener('click', function() {
+        if (this.parentElement.dataset.type === 'temp') {
+            var modal = document.querySelector('.temp-modal');
+            modal.querySelector('.name').innerText = this.querySelector('.name').innerText;
+            modal.querySelector('.status').innerText = this.querySelector('.action').innerText;
+            if (this.parentElement.dataset.power === 'on') {
+                modal.querySelector('.icon-temp').classList.remove('powerOff');
+                modal.querySelector('.icon-temp').classList.add('powerOn');
+            } else {
+                modal.querySelector('.icon-temp').classList.remove('powerOn');
+                modal.querySelector('.icon-temp').classList.add('powerOff');
+            }
+            modal.classList.add('visible');
+        } else if (this.parentElement.dataset.type === 'light') {
+            var modal = document.querySelector('.light-modal');
+            modal.querySelector('.name').innerText = this.querySelector('.name').innerText;
+            modal.querySelector('.status').innerText = this.querySelector('.action').innerText;
+            if (this.parentElement.dataset.power === 'on') {
+                modal.querySelector('.icon-light').classList.remove('powerOff');
+                modal.querySelector('.icon-light').classList.add('powerOn');
+            } else {
+                modal.querySelector('.icon-light').classList.remove('powerOn');
+                modal.querySelector('.icon-light').classList.add('powerOff');
+            }
+            modal.classList.add('visible');
+        }
+    });
+}
+
+/* Закрывать модальное окно регулировки температуры по кнопке Закрыть */
+document.querySelector('.temp-modal .close-modal').addEventListener('click', function() {
+    document.querySelector('.temp-modal').classList.remove('visible');
+    document.querySelector('.page-wrapper').classList.remove('blur');
+});
+
+/* Закрывать модальное окно регулировки температуры по кнопке Применить */
+document.querySelector('.temp-modal .save-modal').addEventListener('click', function() {
+    document.querySelector('.temp-modal').classList.remove('visible');
+    document.querySelector('.page-wrapper').classList.remove('blur');
+});
+
+/* Закрывать модальное окно регулировки света по кнопке Закрыть */
+document.querySelector('.light-modal .close-modal').addEventListener('click', function() {
+    document.querySelector('.light-modal').classList.remove('visible');
+    document.querySelector('.page-wrapper').classList.remove('blur');
+});
+
+/* Закрывать модальное окно регулировки света по кнопке Применить */
+document.querySelector('.light-modal .save-modal').addEventListener('click', function() {
+    document.querySelector('.light-modal').classList.remove('visible');
+    document.querySelector('.page-wrapper').classList.remove('blur');
+});
